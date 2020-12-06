f942c6ab2b9a4234020645a7e66218a8177c1dc3
StephenMike <1576271227@qq.com>
2020-12-06 13:50:04 +0800

    upadte: my-app

---
+++ my-app/src/App.js
@@ -0,0 +1,204 @@
+import React from "react";
+import { Button, Upload, message, Progress } from "antd";
+import "antd/dist/antd.css";
+import typeIntroObj from "./intro";
+import classesJSON from "./output/classes.json";
+
+
+import * as tf from "@tensorflow/tfjs";
+
+const MODEL_DATA_URL = 'http://model.uwayfly.com/model.json' // "http://127.0.0.1:8080/model.json";
+// const CLASSES_DATA_URL = "http://127.0.0.1:8080/classes.json";
+
+function getBase64(img, callback) {
+  const reader = new FileReader();
+  reader.addEventListener("load", () => callback(reader.result));
+  reader.readAsDataURL(img);
+}
+
+function beforeUpload(file) {
+  const isJpgOrPng = file.type === "image/jpeg" || file.type === "image/png";
+
+  if (!isJpgOrPng) {
+    message.error("You can only upload JPG/PNG file!");
+  }
+
+  const isLt2M = file.size / 1024 / 1024 < 2;
+
+  if (!isLt2M) {
+    message.error("Image must smaller than 2MB!");
+  }
+
+  return isJpgOrPng && isLt2M;
+}
+
+function file2img(file) {
+  return new Promise((resolve) => {
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onload = (e) => {
+      const img = document.createElement("img");
+      img.src = e.target.result;
+      img.width = 224;
+      img.height = 224;
+      img.onload = () => resolve(img);
+    };
+  });
+}
+
+function img2X(img) {
+  return tf.tidy(() => {
+    return tf.browser
+      .fromPixels(img)
+      .toFloat()
+      .sub(255 / 2)
+      .div(255 / 2)
+      .reshape([1, 224, 224, 3]);
+  });
+}
+
+class App extends React.PureComponent {
+  constructor(props) {
+    super(props);
+    this.state = {
+      predictRes: [],
+    };
+  }
+
+  async componentDidMount() {
+    this.model = await tf.loadLayersModel(MODEL_DATA_URL);
+    this.model.summary();
+    this.classes = classesJSON; // await fetch(CLASSES_DATA_URL).then((res) => res.json());
+  }
+
+  handleChange = (info) => {
+    if (info.file.status === "uploading") {
+      this.setState({
+        loading: true,
+        imageUrl: null,
+        predictRes: [],
+      });
+      return;
+    }
+
+    // if (info.file.status === "done") {
+    getBase64(info.file.originFileObj, (imageUrl) => {
+      this.setState({
+        imageUrl,
+        loading: false,
+      });
+    });
+    // }
+    console.log(info);
+    this.onPredict(info.file.originFileObj);
+  };
+
+  onPredict = async (file) => {
+    const img = await file2img(file);
+
+    const pred = await tf.tidy(() => {
+      // 输入张量
+      const x = img2X(img);
+      return this.model.predict(x);
+    });
+
+    pred.print();
+
+    const res = pred
+      .arraySync()[0]
+      .map((score, index) => {
+        return {
+          score,
+          label: this.classes[index],
+        };
+      })
+      .sort((a, b) => b.score - a.score);
+
+    console.log(res);
+    this.onHandlePredictResult(res);
+  };
+
+  onHandlePredictResult = (data) => {
+    const predictObj = [];
+    data.map((item) => {
+      if (typeIntroObj[item.label]) {
+        item.icon = typeIntroObj[item.label]["icon"];
+        item.color = typeIntroObj[item.label]["color"];
+        item.intro = typeIntroObj[item.label]["info"];
+        item.percent = parseInt(item.score * 100);
+      }
+    });
+    this.setState({
+      predictRes: data,
+    });
+    console.log(data);
+  };
+
+  render() {
+    const { imageUrl, predictRes } = this.state;
+
+    return (
+      <div className="wrap">
+        <Upload
+          // action="https://www.mocky.io/v2/5cc8019d300000980a055e76"
+          accept="image/*"
+          showUploadList={false}
+          beforeUpload={beforeUpload}
+          onChange={this.handleChange}
+          className="upload"
+        >
+          <Button
+            type="primary"
+            style={{ width: "calc(100vw - 5rem)", marginTop: "1rem" }}
+          >
+            上传
+          </Button>
+        </Upload>
+
+        <div>
+          {imageUrl && (
+            <img src={imageUrl} style={{ width: "100%", marginTop: "1rem" }} />
+          )}
+        </div>
+
+        <div style={{ marginTop: "1rem" }}>
+          <div>识别结果：</div>
+          {!!predictRes.length && (
+            <div style={{ display: "flex" }}>
+              <span>
+                <img className="garbage-type" src={predictRes[0].icon} />
+              </span>
+              <span
+                style={{ marginLeft: "0.2rem", color: predictRes[0].color }}
+              >
+                <h4 style={{ color: predictRes[0].color }}>
+                  {predictRes[0].label}
+                </h4>
+                <span>{predictRes[0].intro}</span>
+              </span>
+            </div>
+          )}
+        </div>
+
+        <div style={{ margin: "1rem 0" }}>
+          <div className="type-row">
+            <span className="type-row-left">类别</span>
+            <span className="type-row-right">匹配度</span>
+          </div>
+          {!!predictRes.length &&
+            predictRes.map((predictItem) => (
+              <div className="type-row" key={predictItem.label}>
+                <span className="type-row-left">{predictItem.label}</span>
+                <span className="type-row-right">
+                  <Progress percent={predictItem.percent} />
+                </span>
+              </div>
+            ))}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
+
---
+++ my-app/src/intro.js
@@ -0,0 +1,24 @@
+export default {
+  "有害垃圾": {
+    icon: 'https://lajifenleiapp.com/static/svg/2v_B43953.svg',
+    color:'#B43953',
+    info: '是指生活垃圾中对人体健康或者自然环境造成直接或者潜在危害的物质，包括废充电电池、废扣式电池、废灯管、弃置药品、废杀虫剂(容器)、废油漆(容器)、废日用化学品、废水银产品、废旧电器以及电子产品等。'
+  },
+  "可回收物": {
+    icon: 'https://lajifenleiapp.com/static/svg/1_3F6BA8.svg',
+    color:'#3F6BA8',
+    info: '是指在日常生活中或者为日常生活提供服务的活动中产生的，已经失去原有全部或者部分使用价值，回收后经过再加工可以成为生产原料或者经过整理可以再利用的物品，包括废纸类、塑料类、玻璃类、金属类、织物类等。'
+  },
+  "厨余垃圾": {
+    icon: 'https://lajifenleiapp.com/static/svg/3v_48925B.svg',
+    color:'#48925B',
+    info: '是指居民日常生活中产生的有机易腐垃圾，包括菜叶、剩菜、剩饭、果皮、蛋壳、茶渣、骨头等。'
+  },
+  "其他垃圾": {
+    icon: 'https://lajifenleiapp.com/static/svg/4_89918B.svg',
+    color:'#89918B',
+    info: '是指除可回收物、有害垃圾和厨余垃圾之外的，混杂、污染、难分类的其他生活垃圾。'
+  },
+
+
+}
